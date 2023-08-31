072c553de68647e678140657fe535414a84993ba
destocot <khurramcali@gmail.com>
2023-08-31 16:44:24 -0400

    made query tests

--- server/database/mutations.js
+++ server/database/mutations.js
@@ -2,18 +2,17 @@ const pool = require('./index.js');
 
 const postReviews = async (data, productId) => {
   const { rating, summary, body, recommend, name, email, helpfulness } = data;
-
   const date = Date.now();
   const values = [productId, rating, date, summary, body, recommend, false, name, email, null, 0];
-
-  console.log(values);
 
   const query = `
     INSERT INTO review
     (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+    RETURNING id, summary
   `;
   const postReviewData = await pool.query(query, values);
+  return postReviewData.rows[0];
 }
 
 const updateHelpfulness = async (reviewId) => {
@@ -21,8 +20,11 @@ const updateHelpfulness = async (reviewId) => {
     UPDATE review
       SET helpfulness = helpfulness + 1
       WHERE id = ($1)
+      RETURNING helpfulness
   `;
   const updateHelpfulnessData = await pool.query(query, [reviewId]);
+  pool.end();
+  return updateHelpfulnessData.rows[0].helpfulness;
 };
 
 module.exports = { postReviews, updateHelpfulness };
--- server/reviewsRoutes.js
+++ server/reviewsRoutes.js
@@ -5,28 +5,28 @@ require('dotenv').config();
 
 const axios = require('axios');
 
-let productId = 27;
+let productId = 30;
 
 reviewsRouter.get('/getAllReviews', async (req, res) => {
   const reviews = await queries.getReviews(productId);
-  res.send(reviews);
+  res.status(200).send(reviews);
 });
 
 reviewsRouter.get('/getRatings', async (req, res) => {
   const meta = await queries.getMeta(productId);
-  res.send(meta);
+  res.status(200).send(meta);
 })
 
 reviewsRouter.post('/reviews', (req, res) => {
   const requestData = req.body;
   mutations.postReviews(requestData, productId);
-  res.send();
+  res.status(201).send();
 });
 
 reviewsRouter.put('/updateHelpful/:review_id', (req, res) => {
   const reviewId = req.params.review_id;
   mutations.updateHelpfulness(reviewId);
-  res.send();
+  res.status(202).send();
 });
 
 module.exports = reviewsRouter;
